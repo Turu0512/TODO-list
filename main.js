2a5b0dea0e69d37c7bb62e13dbd85ee820da0adf
Turu0512 <hinataptyan@gmail.com>
2021-07-06 20:24:10 +0900

    日本語化

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ new Vue({
   el: '#app',
   data: {
     default: '2018-10-04',
-    DatePickerFormat:'yyyy-MM-dd'
+    DatePickerFormat:'yyyy-MM-dd',
+    ja: vdp_translation_ja.js
   },
   components: {
     'vuejs-datepicker':vuejsDatepicker
