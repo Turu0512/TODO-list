ea6a154881f5d76ed66caf2385153bfeef87668f
Turu0512 <hinataptyan@gmail.com>
2021-07-09 20:34:41 +0900

    Todoリスト　最低限の昨日実装

--- main.js
+++ main.js
@@ -16,24 +16,59 @@ var todoStorage = {
 }
 // ↑　ローカルストレージの式
 
-const app = new Vue({
+new Vue({
   el:'#app',
   data: {
-  todos:[],
-  options:[
-  { value:-1, label:'すべて'}, 
-  { value:0, label:'作業中'}, 
-  { value:1, label:'完了'}, 
-  ],
-    // 選択している options の value を記憶するためのデータ
-  // 初期値を「-1」つまり「すべて」にする
-  current:-1
+    todos:[],
+    current:-1,
+    options:[
+    { value:-1, label:'すべて'}, 
+    { value:0, label:'作業中'}, 
+    { value:1, label:'完了'}, 
+    ],
+      // 選択している options の value を記憶するためのデータ
+    // 初期値を「-1」つまり「すべて」にする
+    },
+    
+  computed:{
+    computedTodos:function(){
+            // データ current が -1 ならすべて
+      // それ以外なら current と state が一致するものだけに絞り込む
+
+      return this.todos.filter(function(el){
+        return this.current < 0 ? true:this.current === el.state
+      },this)
+    },
+    labels(){
+        return this.options.reduce(function(a,b){
+          return Object.assign(a,{[b.value]:b.label})
+        },{})
+         // キーから見つけやすいように、次のように加工したデータを作成
+    // {0: '作業中', 1: '完了', -1: 'すべて'}
+      }
+    },
+    
+  watch:{
+    // オプションを使う場合はオブジェクト形式にする
+    todos:{
+      // 引数はウォッチしているプロパティの変更後の値
+    handler:function(todos){
+      todoStorage.save(todos)
+    },
+   // deep オプションでネストしているデータも監視できる
+    deep:true
+    }
   },
+
+  created(){
+    this.todos = todoStorage.fetch()
+  },
+
   methods: {
      // ToDo 追加の処理
     doAdd: function(event,value){
       // ref で名前を付けておいた要素を参照
-      let comment =this.$refs.comment
+      var comment =this.$refs.comment
        // 入力がなければ何もしないで return
       if(!comment.value.length){
         return
@@ -42,7 +77,7 @@ const app = new Vue({
       // というオブジェクトを現在の todos リストへ push
       // 作業状態「state」はデフォルト「作業中=0」で作成
       this.todos.push({
-        id:todosStorage.uid++,
+        id:todoStorage.uid++,
         comment:comment.value,
         state:0
       })
@@ -52,36 +87,12 @@ const app = new Vue({
     },
     // 状態変更の処理
     doChangeState: function(item){
-      item.state = item.state ? 0:1
+      item.state = !item.state ? 0:1
     },
     // 削除の処理
     doRemove: function(item){
-      let index = this.todos.indexOf(item)
+      var index = this.todos.indexOf(item)
       this.todos.splice(index,1)
     }
   },
-  watch:{
-     // オプションを使う場合はオブジェクト形式にする
-    todos:{
-       // 引数はウォッチしているプロパティの変更後の値
-      handler:function(todos){
-        todoStorage.save(todo)
-    },
-     // deep オプションでネストしているデータも監視できる
-    deep:true
-    }
-  },
-  created(){
-    this.todos = todoStorage.fetch()
-  },
-  computed:{
-    computedTodos:function(){
-            // データ current が -1 ならすべて
-      // それ以外なら current と state が一致するものだけに絞り込む
-
-      return this.todos.filter(function(el){
-        return this.current<0 ? true:this.current === el.state
-      },this)
-    }
-  }
 })
